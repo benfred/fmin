91355c01529a162d66549cfc1c6e2d72822843d5
Ben Frederickson <github@benfrederickson.com>
2016-11-24 16:08:21 -0800

    export scale

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ export {bisect} from "./src/bisect";
 export {nelderMead} from "./src/nelderMead";
 export {conjugateGradient} from "./src/conjugateGradient";
 export {gradientDescent, gradientDescentLineSearch} from "./src/gradientDescent";
-export {zeros, zerosM, norm2, weightedSum} from "./src/blas1";
+export {zeros, zerosM, norm2, weightedSum, scale} from "./src/blas1";
